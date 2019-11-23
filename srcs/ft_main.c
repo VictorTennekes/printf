f5a82de98a00dcc2212e3dc50ef1618ea13df0dd
Victor Tennekes <victor@mbp-van-victor.home>
2019-11-23 01:03:36 +0100

    removed objects and added main

---
+++ srcs/ft_main.c
@@ -0,0 +1,10 @@
+#include <ft_printf.h>
+#include <libft.h>
+#include <stdio.h>
+
+int     main(void)
+{
+    ft_printf("dit is een nummer: %i en dit is een hexadecimaal cijfer: %x\n", 54, 69);
+    printf("dit is een nummer: %i en dit is een hexadecimaal cijfer: %x\n", 54, 69);
+    return (0);
+}
